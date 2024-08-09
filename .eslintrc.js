1ec37948c8febb3feffd351855b124ec03468681
dyhes <1325574784@qq.com>
2024-08-09 15:48:29 +0800

    feat: config tailwindcss

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,27 @@
 module.exports = {
   extends: require.resolve('@umijs/max/eslint'),
+  rules: {
+    // You can override specific rules or add new ones here
+    quotes: ['error', 'single', { allowTemplateLiterals: true }],
+  },
+  overrides: [
+    {
+      files: ['*.css', '*.scss', '*.less'], // Adjust this if you're linting CSS files
+      rules: {
+        'at-rule-no-unknown': [
+          0,
+          {
+            ignoreAtRules: [
+              'tailwind',
+              'apply',
+              'variants',
+              'responsive',
+              'screen',
+            ],
+          },
+        ],
+        // other Stylelint rules...
+      },
+    },
+  ],
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,7 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{ts,tsx,js,jsx}'],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
+  content: [
+    './src/pages/**/*.tsx',
+    './src/components/**/*.tsx',
+    './src/layouts/**/*.tsx',
+  ],
 };
